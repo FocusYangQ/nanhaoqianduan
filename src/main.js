d086bea94d12b9072b5ab248de903e7bb8bf8647
FocusYangQ <1098437473@qq.com>
2021-02-08 08:15:09 +0800

    test_login

--- src/main.js
+++ src/main.js
@@ -1,19 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+// import App from './App'
+import Login from './component/Login'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// @指代src
+import '@/assets/css/reset.css'
 
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
+
 new Vue({
-  el: '#app',
+  el: '#app1',
   router,
-  components: { App },
-  template: '<App/>'
+  components: { Login },
+  template: '<Login/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '../component/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
